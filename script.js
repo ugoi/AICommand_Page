fc6b639b1b994ab943cf7f8284d39e188ca2d9db
Stefan Dukic <96357300+UGOI@users.noreply.github.com>
2023-04-04 00:47:27 +0200

    feat: add uninstall section to website

--- script.js
+++ script.js
@@ -16,3 +16,21 @@ function copyToClipboard() {
     }, 1500);
   }
   
+  function copyUninstallCommandToClipboard() {
+    const uninstallCommand = document.getElementById('uninstall-command');
+    const textarea = document.createElement('textarea');
+    textarea.value = uninstallCommand.innerText;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+  
+    // Show a temporary message to indicate the command was copied
+    const copyButton = document.getElementById('uninstall-copy-button');
+    const originalButtonText = copyButton.innerText;
+    copyButton.innerText = 'Copied!';
+    setTimeout(() => {
+      copyButton.innerText = originalButtonText;
+    }, 1500);
+  }
+  
