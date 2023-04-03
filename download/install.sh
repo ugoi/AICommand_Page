94ed276aaba5a6fb3d90f05eaa1e0576a3715cc7
Stefan Dukic <96357300+UGOI@users.noreply.github.com>
2023-04-03 22:45:48 +0200

    fix: install script

--- download/install.sh
+++ download/install.sh
@@ -4,6 +4,12 @@
 curl -o ~/ai_thing.sh https://ugoi.github.io/AICommand_Page/download/ai_thing.sh
 curl -o ~/ai_thing.py https://ugoi.github.io/AICommand_Page/download/ai_thing.py
 
+# Make ai_thing.sh executable
+chmod +x ~/ai_thing.sh
+
+# Make ai_thing.py executable
+chmod +x ~/ai_thing.py
+
 # Add the source command to the appropriate shell configuration file
 if [[ $SHELL == *"zsh"* ]]; then
   echo "source ~/ai_thing.sh" >> ~/.zshrc
