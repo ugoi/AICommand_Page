4bab1ba78facee17cb7aac5d902630523de10e96
Stefan Dukic <96357300+UGOI@users.noreply.github.com>
2023-04-03 22:40:06 +0200

    fix: add copy button to copy command

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// You can add any JavaScript code here if needed
+function copyToClipboard() {
+    const installCommand = document.getElementById('install-command');
+    const textarea = document.createElement('textarea');
+    textarea.value = installCommand.innerText;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+  
+    // Show a temporary message to indicate the command was copied
+    const copyButton = document.getElementById('copy-button');
+    const originalButtonText = copyButton.innerText;
+    copyButton.innerText = 'Copied!';
+    setTimeout(() => {
+      copyButton.innerText = originalButtonText;
+    }, 1500);
+  }
+  
